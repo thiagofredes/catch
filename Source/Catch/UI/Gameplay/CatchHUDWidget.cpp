dc9cbcea456db97e69f8047619fb4cad47c2a1b9
Thiago Rodrigues <11283562+thiagofredes@users.noreply.github.com>
2026-08-21 23:21:18 -0300

    created in-game HUD widget class and blueprint

---
+++ Source/Catch/UI/Gameplay/CatchHUDWidget.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CatchHUDWidget.h"
+#include "Components/TextBlock.h"
+
+
+void UCatchHUDWidget::NativeConstruct()
+{
+}
+
+void UCatchHUDWidget::UpdateTimeRemaining(float NewTime)
+{
+	if (!TimeRemainingTextValue) return;
+
+	const int32 TotalSeconds = FMath::FloorToInt(NewTime);
+	const int32 Minutes = TotalSeconds / 60;
+	const int32 Seconds = TotalSeconds % 60;
+
+	FString TimeString = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+
+	TimeRemainingTextValue->SetText(FText::FromString(TimeString));
+}
+
+void UCatchHUDWidget::UpdateItemCounters(int32 Collected, int32 Remaining)
+{
+	if (CollectedItemsTextValue)
+	{
+		CollectedItemsTextValue->SetText(FText::AsNumber(Collected));
+	}
+	
+	if (RemainingItemsTextValue)
+	{
+		RemainingItemsTextValue->SetText(FText::AsNumber(Remaining));
+	}
+}
